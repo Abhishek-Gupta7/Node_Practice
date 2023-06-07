d3f13a2f46e5fd27f2bd0560078ea43faaacfdca
Abhishek <abhishekgupta@nimapinfotech.com>
2023-06-07 12:17:09 +0530

    Finish with simple Implemention of chat app

--- Project/chatApp/public/Js/script.js
+++ Project/chatApp/public/Js/script.js
@@ -1,15 +1,46 @@
 const socket = io('http://localhost:5000');
 
-let container = document.getElementById('containerMsg');
-let msg = document.getElementById('inputType_msg');
+let send = document.getElementById('send');
+let message = document.getElementById('inputType_msg');
+let container = document.getElementById('container');
 
+let userName = prompt('Enter your name : ');
+appendMsg(`You joined..`);
+
+socket.emit('new-user',userName);
+socket.on('user-connected',(data) => {
+    appendMsg(`${data} joined the chat..`);
+});
+socket.on('user-disconnected',(data) => {
+    appendMsg(`${data} disconnected.`);
+});
 socket.on('chat-message',(data) => {
-    console.log(data);
+    console.log('Data : ',data);
+    appendMsg(`${data.user} : ${data.message}`);
 });
 
-container.addEventListener('click',(e) => {
+send.addEventListener('click',(e) => {
     // e.preventDefault();
-    console.log("e",e);
-    let msg = document.getElementById('inputType_msg').value;
-    console.log("msg",msg);
-})
+    let msg = message.value;
+    // console.log("msg",msg);
+    appendYourMsg(`You : ${msg}`)
+    socket.emit('send-chat-message',msg);
+    message.value = '';
+});
+
+function appendMsg(data){
+    let newElement = document.createElement('div');
+    newElement.innerText = data;
+    newElement.style.backgroundColor = '#fff';
+    container.append(newElement);
+
+}
+
+function appendYourMsg(data){
+    let newElement = document.createElement('div');
+    newElement.innerText = data;
+    newElement.style.backgroundColor = '#e8e9ea';
+    newElement.style.width = 'auto';
+    newElement.style.textAlign = 'right'
+    container.append(newElement);
+}
--- Project/chatApp/start/socket.js
+++ Project/chatApp/start/socket.js
@@ -1,9 +1,24 @@
 
 const io = require('socket.io')(5000,{
     cors:{
-        origin:'*',
+        origin:'http://localhost:3000',
     }
 });
+
+
+const users = {};
 io.on('connection',(socket) => {
-    socket.emit('chat-message','hello..')
+    // socket.emit('chat-message','hello..');
+    socket.on('new-user',(userName) => {
+        users[socket.id] = userName;
+        socket.broadcast.emit('user-connected',userName);
+    })
+    socket.on('send-chat-message',(msg) => {
+        console.log('socket :',msg);
+        socket.broadcast.emit('chat-message',{message :msg ,user : users[socket.id] });
+    })
+    socket.on('disconnect',() => {
+        socket.broadcast.emit('user-disconnected',users[socket.id]);
+        delete users[socket.id];
+    })
 });
